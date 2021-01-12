b4251caa620915fe3eef9440d31f2c76d1781f1c
JerryNing <2422366974@qq.com>
2021-01-12 20:32:21 +0800

    decreased num3

--- 源.cpp
+++ 源.cpp
@@ -5,10 +5,9 @@ int main()
 {
 	int num1 = 0;
 	int num2 = 0;
-	int num3 = 0;
 	int sum = 0;
-	scanf("%d%d", &num1, &num2,&num3);
-	sum = num1 + num2+num3;
+	scanf("%d%d", &num1, &num2);
+	sum = num1 + num2;
 	printf("sum=%d\n", sum);
 
 	return 0;
