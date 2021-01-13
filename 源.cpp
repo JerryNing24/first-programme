c6ec3af28fd6043ec66ce93c9c02bf9f165e88e3
JerryNing <2422366974@qq.com>
2021-01-13 19:14:07 +0800

    deux jour

--- 源.cpp
+++ 源.cpp
@@ -1,15 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
+////int main()
+////{
+////	/*int num1 = 0;
+////	int num2 = 0;
+////	int num3 = 0;
+////	int sum = 0;
+////	scanf("%d%d%d", &num1, &num2,&num3);
+////	sum = num1 + num2 + num3;
+////	printf("sum=%d\n", sum);*/
+////
+////	return 0;
+//}
+//////enum Color
+//////{
+//////	RED,
+//////	YELLOW,
+//////	BLUE,
+//////};
+//////
+//////int main()
+//////{
+//////	enum Color color = BLUE;
+//////	color = YELLOW;
+//////
+//////	return 0;
+//////}
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' ,0};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+////}
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	return 0;
+//}
+//int main()
+////{
+////	printf("abc\n");
+////	return 0;
+////}
 int main()
 {
-	int num1 = 0;
-	int num2 = 0;
-	int num3 = 0;
-	int sum = 0;
-	scanf("%d%d%d", &num1, &num2,&num3);
-	sum = num1 + num2 + num3;
-	printf("sum=%d\n", sum);
-
+	printf("%d\n", strlen("c:\test\32\test.c"));
 	return 0;
 }
