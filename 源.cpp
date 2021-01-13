2a9be2b6d88ee536a7225954cb52e394776354e1
JerryNing <2422366974@qq.com>
2021-01-13 12:27:16 +0800

    added a num3

--- 源.cpp
+++ 源.cpp
@@ -5,9 +5,10 @@ int main()
 {
 	int num1 = 0;
 	int num2 = 0;
+	int num3 = 0;
 	int sum = 0;
-	scanf("%d%d", &num1, &num2);
-	sum = num1 + num2;
+	scanf("%d%d%d", &num1, &num2,&num3);
+	sum = num1 + num2 + num3;
 	printf("sum=%d\n", sum);
 
 	return 0;
