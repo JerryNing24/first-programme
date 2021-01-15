eaab5238d8a99ea5b6b1128e201ffd26243bc5ee
JerryNing <2422366974@qq.com>
2021-01-15 08:28:54 +0800

    troise jours

--- 源.cpp
+++ 源.cpp
@@ -2,17 +2,32 @@
 #include <stdio.h>
 #include <string.h>
 
-////int main()
-////{
-////	/*int num1 = 0;
-////	int num2 = 0;
-////	int num3 = 0;
-////	int sum = 0;
-////	scanf("%d%d%d", &num1, &num2,&num3);
-////	sum = num1 + num2 + num3;
-////	printf("sum=%d\n", sum);*/
-////
-////	return 0;
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d", arr[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int sum = 0;
+//	//scanf("%d%d%d", &num1, &num2);
+//	sum=Add(num1,num2) ;
+//	printf("sum=%d\n", sum);
+//
+//	return 0;
 //}
 //////enum Color
 //////{
@@ -48,8 +63,32 @@
 ////	printf("abc\n");
 ////	return 0;
 ////}
-int main()
-{
-	printf("%d\n", strlen("c:\test\32\test.c"));
-	return 0;
-}
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\32\test.c"));
+//	return 0;
+//}
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？（1/0)>:");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line < 20000)
+//	{
+//		printf("敲一行代: % d\n", line);
+//		line++;
+//	}
+//	if (line >= 20000)
+//		printf("好offer\n");
+//	return 0;
